60de6bf890a626e421e0da2ef37d35f141554350
Subhash Kumar <subhashk1508@gmail.com>
2025-05-15 00:51:27 +0530

    project link added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,13 +8,13 @@ function Projects() {
         <div className="flex flex-wrap justify-center items-center font-bold  font-serif text-5xl text-rose-950 mt-10">Projects</div>
         <div className='h-auto w-full flex flex-wrap text-white justify-center mt-5 md:mt-10'>
 
-           <Link to="https://subhashkr15.github.io/Amazone-Clone/">
+           <Link to="https://amazone-clone-blue.vercel.app/">
             <div className="h-80 w-80 md:w-150 md:h-80 border-2 border-gray-600 rounded-4xl m-2 bg-[url('/Images/amazon.png')]  bg-cover mx-3 " >
               <p className='font-bold text-2xl flex justify-center'>Amazon Clone</p>
             </div>
             </Link>
 
-            <Link to="https://subhashkr15.github.io/watchPaglu/">
+            <Link to="https://watch-paglu-tau.vercel.app/">
             <div className="h-80 w-80 md:w-150 md:h-80 border-2 border-gray-600 mx-3 rounded-4xl m-2 bg-[url('/Images/watchPaglu.png')] bg-cover bg-center" > 
               <p className='font-bold text-2xl flex justify-center'>WatchPaglu</p>
             </div>
