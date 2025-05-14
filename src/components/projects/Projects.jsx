5bde46590c553f1b65ff215c99aa97406f461255
Subhash Kumar <subhashk1508@gmail.com>
2025-05-15 00:05:34 +0530

    project link added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Projects() {
   return (
-    <div>
+      <div>
       
         <div className="flex flex-wrap justify-center items-center font-bold  font-serif text-5xl text-rose-950 mt-10">Projects</div>
-        <div className='h-auto w-full  flex flex-wrap text-white justify-center mt-5 md:mt-10'>
-        <div className="h-80 w-120 border-2 border-gray-600 rounded-4xl m-2 bg-[url('/Images/amazon-img.png')]  bg-cover mx-3 cursor-pointer" >
-          <p className='font-bold text-2xl flex justify-center'>Amazon Clone</p>
-        </div>
-        <div className="h-80 w-120 border-2 border-gray-600 mx-3 rounded-4xl m-2 bg-[url('/Images/watchPaglu.png')] bg-cover bg-center cursor-pointer " > 
-          <p className='font-bold text-2xl flex justify-center'>WatchPaglu</p></div>
-        
-        <div className="h-80 w-120 border-2 border-gray-600 md:mt-3 md:mb-4 m-2 md:mx-3 rounded-4xl  bg-[url('/Images/currency-img.png')] bg-cover bg-center cursor-pointer " >
-        <p className='font-bold text-2xl flex justify-center'>Currency Converter</p>
-        </div>
-        <div className="h-80 w-120  border-2 border-gray-600 md:mt-3 md:mb-4 mx-3 m-2 rounded-4xl bg-[url('/Images/blog-img.png')]  bg-cover  cursor-pointer" >
-           <p className='font-bold text-2xl flex justify-center '>Blog Space</p></div>
-    </div>
+        <div className='h-auto w-full flex flex-wrap text-white justify-center mt-5 md:mt-10'>
+
+           <Link to="https://subhashkr15.github.io/Amazone-Clone/">
+            <div className="h-80 w-120 hover:w-160 border-2 border-gray-600 rounded-4xl m-2 bg-[url('/Images/amazon.png')]  bg-cover mx-3 " >
+              <p className='font-bold text-2xl flex justify-center'>Amazon Clone</p>
+            </div>
+            </Link>
+
+            <Link to="https://subhashkr15.github.io/watchPaglu/">
+            <div className="h-80 w-120 hover:w-160 border-2 border-gray-600 mx-3 rounded-4xl m-2 bg-[url('/Images/watchPaglu.png')] bg-cover bg-center" > 
+              <p className='font-bold text-2xl flex justify-center'>WatchPaglu</p>
+            </div>
+            </Link>
+
+            <Link to="https://currency-generator-eight.vercel.app/">
+            <div className="h-80 w-120 hover:w-160 border-2 border-gray-600 md:mt-3 md:mb-4 m-2 md:mx-3 rounded-4xl  bg-[url('/Images/currency.png')] bg-cover bg-center" >
+            <p className='font-bold text-2xl flex justify-center'>Currency Converter</p>
+            </div>
+            </Link>
+
+            <div className="h-80 w-120 hover:w-160 border-2 border-gray-600 md:mt-3 md:mb-4 mx-3 m-2 rounded-4xl bg-[url('/Images/blog-img.png')]  bg-cover" >
+              <p className='font-bold text-2xl flex justify-center '>Blog Space</p>
+            </div>
+         </div>
     </div>
   )
 }
